4bc8baf2558bb10af765973c42c35fd789e63c22
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2021-06-03 00:55:38 +0300

    Use an early return if there is no cookie

--- src/lib/cookie.js
+++ src/lib/cookie.js
@@ -3,8 +3,8 @@ export function getCookie (name) {
   const prefix = `${encodeURIComponent(name)}=`
   const cookie = cookies.find(cookie => cookie.startsWith(prefix))
 
-  if (cookie) {
-    const value = cookie.split('=').slice(1).join('=')
-    return value ? decodeURIComponent(value) : undefined
-  }
+  if (!cookie) return
+
+  const value = cookie.split('=').slice(1).join('=')
+  return value ? decodeURIComponent(value) : undefined
 }
