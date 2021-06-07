41729de14b62eed5bd6f8870374206682b9595e6
Adrien Poly <adrienpoly@gmail.com>
2021-06-08 01:20:12 +0200

    update Request to FetchRequest

--- src/verbs.js
+++ src/verbs.js
@@ -1,27 +1,27 @@
-import { Request } from './request'
+import { FetchRequest } from './fetchRequest'
 
 async function get (url, options) {
-  const response = new Request('get', url, options)
+  const response = new FetchRequest('get', url, options)
   return response.perform()
 }
 
 async function post (url, options) {
-  const response = new Request('post', url, options)
+  const response = new FetchRequest('post', url, options)
   return response.perform()
 }
 
 async function put (url, options) {
-  const response = new Request('put', url, options)
+  const response = new FetchRequest('put', url, options)
   return response.perform()
 }
 
 async function patch (url, options) {
-  const response = new Request('patch', url, options)
+  const response = new FetchRequest('patch', url, options)
   return response.perform()
 }
 
 async function destroy (url, options) {
-  const response = new Request('delete', url, options)
+  const response = new FetchRequest('delete', url, options)
   return response.perform()
 }
 
