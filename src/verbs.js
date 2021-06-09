d00f82860734c96f88684ee8c7c06c7bf13f2d5f
Michael Josephson <30024+mikej@users.noreply.github.com>
2021-06-09 08:47:55 +0100

    rename request objects in verbs.js for clarity
    these are the request object rather than the response so naming as
    `request` feels clearer.

--- src/verbs.js
+++ src/verbs.js
@@ -1,28 +1,28 @@
 import { FetchRequest } from './fetch_request'
 
 async function get (url, options) {
-  const response = new FetchRequest('get', url, options)
-  return response.perform()
+  const request = new FetchRequest('get', url, options)
+  return request.perform()
 }
 
 async function post (url, options) {
-  const response = new FetchRequest('post', url, options)
-  return response.perform()
+  const request = new FetchRequest('post', url, options)
+  return request.perform()
 }
 
 async function put (url, options) {
-  const response = new FetchRequest('put', url, options)
-  return response.perform()
+  const request = new FetchRequest('put', url, options)
+  return request.perform()
 }
 
 async function patch (url, options) {
-  const response = new FetchRequest('patch', url, options)
-  return response.perform()
+  const request = new FetchRequest('patch', url, options)
+  return request.perform()
 }
 
 async function destroy (url, options) {
-  const response = new FetchRequest('delete', url, options)
-  return response.perform()
+  const request = new FetchRequest('delete', url, options)
+  return request.perform()
 }
 
 export { get, post, put, patch, destroy }
